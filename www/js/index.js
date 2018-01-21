1f2af1e5b57667ed160d00bac6a515373e05c910
Louis Larivière <louis.lariviere@gmail.com>
2018-01-20 23:30:13 -0500

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -17,30 +17,6 @@ $$(document).on('deviceready', function() {
 });
 
 
-// Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-
-})
-
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
-    // Get page data from event data
-    var page = e.detail.page;
-
-    if (page.name === 'about') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
-    }
-})
-
-// Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
-})
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -61,13 +37,6 @@ var app = {
   // Update DOM on a Received Event
   receivedEvent: function(id) {
     var parentElement = document.getElementById(id);
-    var listeningElement = parentElement.querySelector('.listening');
-    var receivedElement = parentElement.querySelector('.received');
-
-    listeningElement.setAttribute('style', 'display:none;');
-    receivedElement.setAttribute('style', 'display:block;');
-
-    console.log('Received Event: ' + id);
   }
 
 };
@@ -76,7 +45,7 @@ app.initialize();
 
 
 var currentDocumentImage = { imageFileUri: '', originalImageFileUri: '' };
-var ocrLanguages = ["en", "de"];
+var ocrLanguages = ["en", "fr"];
 var jpgQuality = 70;
 
 
